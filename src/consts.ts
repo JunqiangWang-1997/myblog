2bc0aa9ce54cddd12d77ffa983340541827aa782
JunqiangWang <774240456@qq.com>
2026-02-15 22:16:20 +0800

    feat: add CodeBlock component and create blog post on Cloudflare Workers reverse proxy for Emby

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Junqiang Wang的博客';
+export const SITE_DESCRIPTION = '欢迎来到我的博客，这里分享我的技术文章和个人见解。';
